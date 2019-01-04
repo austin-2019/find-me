3d06bd7460588f5eff813cd86b11f8f1aabda2b1
austin-2019 <sharon.austin.2019@gmail.com>
2019-01-03 23:01:30 -0500

    Unable to activate webcam, although lat-long is working

---
+++ public/js/picture.js
@@ -0,0 +1,9 @@
+function hasGetUserMedia() {
+    return !!(navigator.mediaDevices &&
+      navigator.mediaDevices.getUserMedia);
+  }
+  if (hasGetUserMedia()) {
+    // Good to go!
+  } else {
+    alert('getUserMedia() is not supported by your browser');
+  }
