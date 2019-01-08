7f6a77743960897217a92b591176034b29f30a48
austin-2019 <sharon.austin.2019@gmail.com>
2019-01-07 22:32:51 -0500

    added firebasesdk.js to be able to retrieve data from firebase

---
+++ firebasesdk.js
@@ -0,0 +1,9 @@
+/* https://firebase.google.com/docs/admin/setup#add_firebase_to_your_app */
+var admin = require('firebase-admin');
+
+var serviceAccount = require('path/to/serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
+});
