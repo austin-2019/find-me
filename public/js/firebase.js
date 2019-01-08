3b808869e642cbe6cf7e0cbf64fb61227c84fdc1
austin-2019 <sharon.austin.2019@gmail.com>
2019-01-07 21:25:48 -0500

    getting info but still not sending

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,35 +1,56 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD123PvPBnD0L4Nw6DKvlyjPzYIE9Sx_Jo",
+    authDomain: "find-me-eba22.firebaseapp.com",
+    databaseURL: "https://find-me-eba22.firebaseio.com",
+    projectId: "find-me-eba22",
+    storageBucket: "find-me-eba22.appspot.com",
+    messagingSenderId: "511040371879"
+  };
+  firebase.initializeApp(config);
 
-$.ajax({
-  url: "/dataFromFirebase",
-  success: function (result) {
-    let parsedResult = JSON.parse(result);
 
+// Reference messages collection
+var messagesRefNoCamera = firebase.database().ref('messages');
 
-    if (parsedResult.fbdbimage) {
-      $("#fbdbimage").text("Image Credits: " + parsedResult.fbdbimage);
-    }
-    else {
-      $("#fbdbimage").text("Image Credits: " + "Public Domain");
-      console.log(parsedResult);
-    }
+// Listen for form submit
+document.getElementById('nocameraform').addEventListener('submit', submitNoCameraForm);
 
-    if (parsedResult.media_type == "video") {
-      $("#fbvideo_img_id").css("display", "none");
-      $("#fbvideo_vid_id").attr("src", parsedResult.url);
-    }
-    
-    else {
-      $("#fbother_vid_id").css("display", "none");
-      $("#fbother_img_id").attr("src", parsedResult.url);
-    }
-    //$("#reqObject").text('Hello' + result.explanation);
-    
-    $("#reqObjectFB").text('Hello' + parsedResult);
-    $("#reqObjectFB").text(parsedResult.url);
-    
-    //$("#returnObject").text(JSON.stringify(result, null, 4));
-    $("#fb_explanation").text(parsedResult.explanation);
-    $("#fb_title").text(parsedResult.title);
-  }
+// Submit form
+function submitNoCameraForm(e){
+  e.preventDefault();
+  var latcoords = getInputValNoCamera('latcoords');
+  var longcoords = getInputValNoCamera('longcoords'); 
+  var name = getInputValNoCamera('name');
+  var phone = getInputValNoCamera('phone');
+  var myHiddenField = getInputValNoCamera('myHiddenField');
   
-});
+  saveMessageNoCamera(name, phone, latcoords, longcoords, myHiddenField);
+  
+    // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+  
+  document.getElementById('nocameraform').reset();
+} 
+
+// Function to get get form values
+function getInputValNoCamera(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessageNoCamera(name, phone, latcoords, longcoords, myHiddenField){
+  var newMessageRefNoCamera = messagesRefNoCamera.push();
+  newMessageRefNoCamera.set({
+    name: name,
+    phone:phone,
+    latcoords: latcoords,
+    longcoords:longcoords,
+    myHiddenField:myHiddenField
+  });
+}
