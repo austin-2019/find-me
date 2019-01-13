4b908a20b6fda10091ceabff0fcbc232c609ed69
austin-2019 <sharon.austin.2019@gmail.com>
2019-01-12 21:10:02 -0500

    Setting up to retrieve data still have not resolved append issue for TextMessages

--- public/js/userDataref.js
+++ public/js/userDataref.js
@@ -1,14 +1,21 @@
 /* currently database is over-writing out current data check for append */
 /* based on stackoverflow questions/42684712/display-data-from-firebase-database-in-a-html-page */
-var userDataRef = firebase.database().ref("UserData").orderByKey();
+/* https://stackoverflow.com/questions/38541098/how-to-retrieve-data-from-firebase-database */
+var messagesDataRef = firebase.database().ref("messages").orderByKey();
 userDataRef.once("value").then(function(snapshot){
     snapshot.forEach(function(childSnapshot){
         var key = childSnapshot.key;
         var childData = childSnapshot.val();
 
+        var latcoords_val = childSnapshot.val().LatCoords;
+        var longcoords_val = childSnapshot.val().LongCoords;
+        var myhiddenfield_val = childSnapshot.val().MyHiddenField;
         var name_val = childSnapshot.val().Name;
-        var id_val = childSnapshot.val().AssignedID;
-        $("#name").append(name_val);
-        $("#id").append(id_val);
+        var phone_val = childSnapshot.val().Phone;
+        $("#displaylat").append(latcoords_val);
+        $("#displaylong").append(longcoords_val);
+        $("#displayhiddenfield").append(name_val);
+        $("#displayname").append(name_val);
+        $("#displayphone").append(phone_val);
     });
 });
